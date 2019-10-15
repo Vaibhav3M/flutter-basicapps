984c4ae25a24a4666718440aed3f855b161772de
Vaibhav3M <44673587+Vaibhav3M@users.noreply.github.com>
2019-10-15 04:32:07 -0400

    Added score validation

--- quiz_app/lib/Quiz.dart
+++ quiz_app/lib/Quiz.dart
@@ -5,11 +5,14 @@ import 'package:quiz_app/Answer.dart';
 
 class Quiz extends StatelessWidget {
 
-  final questionIndex;
-  final questions;
+  final int questionIndex;
+  final List<Map<String, Object>>questions;
   final Function answerQuestion;
+  var score = 0;
 
-  Quiz(this.questions,this.questionIndex,this.answerQuestion);
+  Quiz({@required this.questions,
+      @required this.questionIndex,
+      @required this.answerQuestion});
 
   @override
   Widget build(BuildContext context) {
@@ -18,8 +21,8 @@ class Quiz extends StatelessWidget {
             Question(
               questions[questionIndex]["questionText"],
             ),
-            ...(questions[questionIndex]['answerList'] as List<String>).map((answer) {
-              return Answer(answerQuestion,answer);
+            ...(questions[questionIndex]['answerList'] as List<Map<String,Object>>).map((answer) {
+              return Answer(() => answerQuestion(answer['score']),answer['text']);
             }).toList(),
           ],
         );
--- quiz_app/lib/main.dart
+++ quiz_app/lib/main.dart
@@ -12,7 +12,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Quiz Demo',
       theme: ThemeData(
-
         primarySwatch: Colors.blue,
       ),
       home: HomePage(),
@@ -20,53 +19,78 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatefulWidget{
-
+class HomePage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-
     return HomePageState();
   }
-
 }
 
-class HomePageState extends State<HomePage>{
-
+class HomePageState extends State<HomePage> {
   var _questionIndex = 0;
+  var _totalScore =0;
 
   final questions = const [
     {
-      "questionText" : "What's your favourite Color?",
-      "answerList" : ['Red','Yellow','Green','Blue'],
+      "questionText": "What's your favourite Color?",
+      "answerList": [
+        {'text': 'Red', 'score': 10},
+        {'text': 'Yellow', 'score': 15},
+        {'text': 'Green', 'score': 20},
+        {'text': 'Blue', 'score': 15}
+      ],
     },
     {
-      "questionText" : "What's is your favourite Animal?",
-      "answerList" : ['Rabbit','Snake','Elephant','Dog'],
+      "questionText": "What's is your favourite Animal?",
+      "answerList": [
+        {'text': 'Rabbit', 'score': 10},
+        {'text': 'Snake', 'score': 5},
+        {'text': 'Elephant', 'score': 20},
+        {'text': 'Dog', 'score': 30}
+      ],
     },
     {
-      "questionText" : "What's is your favourite Food?",
-      "answerList" : ['Pizza','Oats','Burger','Pasta'],
-    }
+      "questionText": "What's is your favourite Food?",
+      "answerList": [
+        {'text': 'Pizza', 'score': 10},
+        {'text': 'Oats', 'score': 30},
+        {'text': 'Burger', 'score': 10},
+        {'text': 'Pasta', 'score': 20}
+      ],
+    },
   ];
 
+  void _answerQuestion(int score) {
 
-  void _answerQuestion(){
-
+    _totalScore += score;
     setState(() {
       _questionIndex++;
     });
   }
 
+  void _resetQuiz(){
+
+    setState(() {
+      _questionIndex =0;
+      _totalScore = 0;
+    });
+
+
+  }
+
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-            "Quiz Demo"),
+        title: Text("Quiz Demo"),
       ),
       // added check for length of question - display widget in respect to that
-      body: _questionIndex < questions.length ? Quiz(questions, _questionIndex, _answerQuestion): Result(),
+      body: _questionIndex < questions.length
+          ? Quiz(
+              questions: questions,
+              questionIndex: _questionIndex,
+              answerQuestion: _answerQuestion)
+          : Result(_totalScore,_resetQuiz),
     );
   }
 }
