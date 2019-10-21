b23707e97790468a70e4ac7bc191f49d81df71ab
Vaibhav3M <44673587+Vaibhav3M@users.noreply.github.com>
2019-10-21 04:14:04 -0400

    added category details data

--- resturant_planner/lib/Views/category_details_screen.dart
+++ resturant_planner/lib/Views/category_details_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+
+import 'package:resturant_planner/model/Meal.dart';
+
 class CategoryDetailsScreen extends StatelessWidget {
 //  final String id;
 //  final String title;
@@ -11,13 +14,26 @@ class CategoryDetailsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final routArgs = ModalRoute.of(context).settings.arguments as Map<String,String>;
+    final id = routArgs['id'];
     final title = routArgs['title'];
+
+    final categoryMeals = DUMMY_MEALS.where((meal){
+
+      return meal.categories.contains(id);
+    }).toList();
+
 
     return Scaffold(
       appBar: AppBar(
         title: Text(title),
       ),
-      body: Center(child: Text(title)),
+      body: ListView.builder(itemBuilder: (ctx,index){
+
+          return Text(categoryMeals[index].title);
+      },
+      itemCount: categoryMeals.length,
+
+      ),
     );
   }
 }
